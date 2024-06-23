c0dd41258f9e62028003e65f1f3f827c45950029
Surya Gs <surya.gs@pwc.com>
2024-06-23 21:45:45 +0530

    added additional functions

---
+++ python/dataypes/string/dict.py
@@ -0,0 +1,7 @@
+#A Dictonaty is a collection which is unordered, changable and indexded
+
+#dict()
+
+x=dict(name="slnojog", sjihihg="23")
+print(x)
+
---
+++ python/dataypes/string/dir.py
@@ -0,0 +1,9 @@
+#Dir functions returns all properties and methods of specifiede objects
+
+#The dir() function returns all properties and methods of the specified object, without the values.
+
+class person:
+    name= "dfg"
+    age=34
+print(dir(person))
+
