b1aa77ef5352d88834ddaa795e6743e3e32316a7
Surya Gs <surya.gs@pwc.com>
2024-06-29 14:35:09 +0530

    lists and tuples

---
+++ python/sets_dicts/dict.py
@@ -0,0 +1,9 @@
+my_dict1 = {
+    'name': 'Alice',
+    'age': 25,
+    'occupation': 'Software Engineer'
+}
+print(my_dict1['name'])
+
+my_dict = dict([('name', 'Alice'), ('age', 25)])
+print(my_dict)
---
+++ python/sets_dicts/readme.md
@@ -0,0 +1,10 @@
+Sets and Dictonaries
+
+
+Set:
+A set in Python is an unordered collection of unique elements.
+
+
+
+Dictonaries
+A dictionary in Python is an ordered collection of key-value pairs
