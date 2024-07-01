56caffb7544c8d8c91f099b9b9336954593c9d48
Surya Gs <surya.gs@pwc.com>
2024-07-01 12:22:47 +0530

    added example for lista and sets

---
+++ python/problems/2.py
@@ -0,0 +1,8 @@
+# 
+student_info={
+    "name": "age",
+    "age": 10,
+}
+
+
+print(student_info)
---
+++ python/problems/3.py
@@ -0,0 +1,23 @@
+#list of dictonaries
+
+ec2_instance_type=[
+    {
+        "name":"ec2-instance-001",
+        "type":"micro"
+    },
+    {
+        "name":"ec2-instance-002",
+        "type":"medium"
+    },
+    {
+        "name":"ec2-instance-003",
+        "type":"large"
+    },
+]
+
+print(ec2_instance_type[2]["type"])
+
+
+#the above code contains the sample code to list the dictonaires considering ec2 instance examples
+
+# Dict are  used to solve complex problems
